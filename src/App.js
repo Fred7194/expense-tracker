4b3403edbd265f01fee67ea5f78644c6c7d03b58
Fred Moore <fmoore1124@gmail.com>
2020-10-14 10:21:46 -0400

    state coming into components next setup actions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { Header } from './components/Header'
 import { Balance } from './components/Balance'
 import { IncomeExpenses } from './components/IncomeExpenses'
-import './App.css'
 import { TransactionList } from './components/TransactionList'
 import { AddTransaction } from './components/AddTransaction'
 
+import { GlobalProvider } from './context/GlobalState'
+
+import './App.css'
+
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className='container'>
         <Balance />
@@ -16,7 +19,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
